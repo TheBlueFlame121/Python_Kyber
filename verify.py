35550c2713ee09e6d211259b224a470365ea24e2
TheBlueFlame121 <pranshukumar27@gmail.com>
2023-06-15 23:39:03 +0530

    Initial verify.py

---
+++ verify.py
@@ -0,0 +1,11 @@
+# Contains elements from verify. These are rewritten because constant time does not hold for python anyway.
+from typing import List
+
+def verify(a:List[int], b:List[int], l:int):
+    return all([i == j for i, j in zip(a, b)])
+
+
+def cmov(r:List[int], x:List[int], l:int, b:int):
+    if b == 1:
+        for i in range(l):
+            r[i] = x[i]
